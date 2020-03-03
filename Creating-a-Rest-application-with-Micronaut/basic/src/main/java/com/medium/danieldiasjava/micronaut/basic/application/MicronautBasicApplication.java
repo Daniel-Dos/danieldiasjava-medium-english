6a6356fe93ec9eae9c45d819f1623170700c9267
Daniel Dias <daniel.dias.analistati@gmail.com>
2020-03-03 18:11:04 -0300

    basic project .

---
+++ Creating-a-Rest-application-with-Micronaut/basic/src/main/java/com/medium/danieldiasjava/micronaut/basic/application/MicronautBasicApplication.java
@@ -0,0 +1,9 @@
+package com.medium.danieldiasjava.micronaut.basic.application;
+
+import io.micronaut.runtime.Micronaut;
+
+public class MicronautBasicApplication {
+    public static void main(String[] args) {
+        Micronaut.run(MicronautBasicApplication.class, args);
+    }
+}
